157f0ca2d5ef94f17078a6cca5b12b38f839bf49
skshm <eminate894567@protonmail.com>
2022-12-31 15:42:25 +0530

    created moduled ver

--- noise_cancel.py
+++ noise_cancel.py
@@ -1,19 +1,33 @@
-# %%
 from scipy.io import wavfile
 import noisereduce as nr
 import soundfile as sf
 from noisereduce.generate_noise import band_limited_noise
 
-# %%
 
-data, rate = sf.read("Cancelation/audios/Wherever.wav")
-data = data
-# %%
-reduced_stat = nr.reduce_noise(y = data.T, sr=rate, n_std_thresh_stationary=1.5,stationary=True)
+def callAudio(audio):
+    data, rate = sf.read(audio)
+    return data, rate
 
-# %%
-reduced_nonstat = nr.reduce_noise(y = data.T, sr=rate, thresh_n_mult_nonstationary=2,stationary=False)
+def statCall(data, rate):
+    reduced_stat = nr.reduce_noise(y = data.T, sr=rate, n_std_thresh_stationary=1.5,stationary=True)
+    return reduced_stat.T
 
-# %%
-wavfile.write("Cancelation/tests/Wherever_stationary.wav", rate, reduced_stat.T)
-wavfile.write("Cancelation/tests/Wherever_NonStationary.wav", rate, reduced_nonstat.T)
+
+def nonstatCall(data, rate):
+    reduced_nonstat = nr.reduce_noise(y = data.T, sr=rate, thresh_n_mult_nonstationary=2,stationary=False)
+    return reduced_nonstat.T
+
+def saver(type, rate, name):
+    wavfile.write(f"Cancelation/tests/{name}.wav", rate, type)
+
+def main():
+    aData, aRate = callAudio("Cancelation/audios/Wherever.wav")
+    stat = statCall(aData, aRate)
+    nonstat = nonstatCall(aData, aRate)
+    
+    saver(stat, aRate, "Wherever_stat")
+    saver(nonstat, aRate, "Wherever_nonstat")
+    
+
+if __name__ == "__main__":
+    main()
