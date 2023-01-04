ce6d2ae89d733219a9dab9fefd71ac7ce9e15573
skshm <eminate894567@protonmail.com>
2023-01-04 10:01:09 +0530

    forgot to call function

--- record_audio.py
+++ record_audio.py
@@ -3,19 +3,24 @@ import sounddevice as sd
 from scipy.io.wavfile import write
 import wavio as wv
 
-def recorder(freq,duration,path):
-# Start recorder with the given values of 
-# duration and sample frequency
-recording = sd.rec(int(duration * freq), 
+
+def recorder(freq, duration, path):
+
+
+    # Start recorder with the given values of
+    # duration and sample frequency
+    recording = sd.rec(int(duration * freq),
                    samplerate=freq, channels=2)
 
-# Record audio for the given number of seconds
-sd.wait()
-# This will convert the NumPy array to an audio
-# file with the given sampling frequency
-write(f"{path}_init.wav", freq, recording)
-# Convert the NumPy array to audio file
-wv.write(f"{path}_final.wav", recording, freq, sampwidth=2)
+    # Record audio for the given number of seconds
+    sd.wait()
+    
+    # This will convert the NumPy array to an audio
+    # file with the given sampling frequency
+    write(f"{path}_init.wav", freq, recording)
+    # Convert the NumPy array to audio file
+    wv.write(f"{path}_final.wav", recording, freq, sampwidth=2)
+
 
 def main():
     # Sampling frequency
@@ -23,5 +28,9 @@ def main():
     # Recording duration
     duration = 5
     path = "recording/audioData"
+    
+    recorder(freq, duration, path)
+
+
 if __name__ == "__main__":
     main()
